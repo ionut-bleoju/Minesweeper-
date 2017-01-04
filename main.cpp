8ab74ea70f4b8e218a7ede039360ba06c7c57d16
Bleoju Ionut Bogdan <ionutyop@gmail.com>
2017-01-04 23:01:28 +0200

    Inregistrare actiune
    inregistrare actiune sub forva coord x y si actiunea 1/2

--- main.cpp
+++ main.cpp
@@ -46,6 +46,28 @@ void Traseaza(string icons)
 	cout << "|" << endl;
 }
 
+void Actiune()
+	{actiune[0] = actiune[1] = actiune[2] = 0;
+	cout
+	<< endl << "[" << icons.at(9) << "] - necunoscut   [" << icons.at(1) << "-" << icons.at(8) << "] - # mine inprejur"
+	<< endl << "[" << icons.at(0) << "] - 0 mine  [" << icons.at(10) << "] - mine  [" << icons.at(11) << "] - Marcat"
+	<< endl << "-Miscare 1-Identifica 2-Marcheaza";
+
+	do
+	{
+		cout << endl << endl << "Actiune = ";
+		cin >> actiune[0] >> actiune[1] >> actiune[2];
+		cout << endl << "Valideaza...";
+	} while (!testactiune());//De inplementat
+	if (actiune[2] == 1)
+	{
+		Investigheaza();//De inplementat
+	}
+	else
+	{
+		Marcheaza();//De inplementat
+	}
+}
 
 int main()
 {cin>>dim[0];
