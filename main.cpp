8bb427765da51a5679fef73aca1394cb8ceb3cec
Bleoju Ionut Bogdan <ionutyop@gmail.com>
2017-01-05 16:59:00 +0200

    Inregistrare actiune + verificare corectitudine
    Inregistrare actiune si verificarea acesteia daca este in parametri
    corespunzatori.

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@ using namespace std;
 //variabile globale
 int dim[2];//Dimensiune tabela ;
 int tabel[15][15];
+int actiune[3];
+int mine = 0;
+bool  test = true;
 const string icons = " 12345678++%%*";
-
+#define limita(X,NUM) (NUM<=dim[X] && NUM>0)
 
 void Traseaza(string icons)
 {
 	cout<<endl<< "  |";
-	for (int i=0;i<dim[0];i++) //Prima Linie
+	for (int i=0;i<dim[0];i++)
 	{
 			cout << "----";
 	}
@@ -46,6 +49,56 @@ void Traseaza(string icons)
 	cout << "|" << endl;
 }
 
+
+
+bool testactiune()
+{
+	bool OK = (limita(0,actiune[0]) && limita(1,actiune[1]) && actiune[2]<=2 && actiune[2]>0);
+	if (OK)
+	{
+		int *titlu = &tabel[actiune[0]-1][actiune[1]-1];
+		if (*titlu > -1 && *titlu < 9) //Spatiul este deja inspectat sau marcat
+
+		{
+			cout << endl << "Actiunea nu este permisa " << actiune[0] << " " << actiune[1];
+			OK = false;
+		}
+		else
+		{
+			cout << endl << "Permisa " << endl;
+			if (actiune[2] == 1)
+			{
+				cout << "Actiune - Identifica";
+			}
+			else
+			{
+				cout << "Actiune - Marcheaza";
+			}
+			cout <<  " @ X = " << actiune[0] << ", Y = " << actiune[1];
+		}
+	}
+	else
+	{
+		cout << endl << "Eroare:Valoarea introdusa este inafara domeniului";
+	}
+	return OK;
+}
+
+void Investigheaza()
+{
+	cout << endl << "Inspecteaza...";
+	int *tile = &tabel[actiune[0]-1][actiune[1]-1];
+	if (*tile == 10 || *tile == 12)
+	{
+		test = false;
+		*tile = 13;
+	}
+	else
+	{
+		extinde(actiune[0]-1, actiune[1]-1);
+	}
+}
+
 void Actiune()
 	{actiune[0] = actiune[1] = actiune[2] = 0;
 	cout
@@ -70,9 +123,6 @@ void Actiune()
 }
 
 int main()
-{cin>>dim[0];
- cin>>dim[1];
-
-   Traseaza(icons);
+{
     return 0;
 }
